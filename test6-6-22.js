8ddcef0099b7db6627a1afdae407be0759845554
AMAN270899 <102337810+AMAN270899@users.noreply.github.com>
2022-06-06 07:28:51 +0530

    Add files via upload

---
+++ test6-6-22.js
@@ -0,0 +1,9 @@
+let n= parseInt(prompt('Enter the number'))
+function DisplayNTermsOfOddNumbersAndTheirSum(n) {
+let sum=0;
+    for(leti=1; i<=n; i+=2); 
+    sum=sum+i;
+    console.log(i);
+    console.log(sum);
+}
+DisplayNTermsOfOddNumbersAndTheirSum()
